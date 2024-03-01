83d26188991ada1029083aa4f8dc059098ef145b
Lakkas <abdel-waheb.sakkal@efrei.net>
2024-03-01 14:53:21 +0100

    harmonization of the code with OOP

---
+++ automaton.py
@@ -0,0 +1,73 @@
+
+class Automaton:
+
+    def __init__(self, filename):
+        self.alphabet = None
+        self.states = None
+        self.initals_states = None
+        self.terminal_states = None
+        self.transitions = None
+        self.initialize(filename)
+
+    def initialize(self, filename):
+        
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            alphabet_size = int(lines[0])
+            nb_states = int(lines[1])
+            states = list(range(nb_states))  # Crée une liste d'états
+            self.initals_states = list(map(int, lines[2].split()[1:]))
+            self.terminal_states = list(map(int, lines[3].split()[1:]))
+            transitions = []
+            for line in lines[5:]:
+                transition_data = line.strip()
+                start_state = int(transition_data[0])
+                symbol = transition_data[1]
+                terminal_state = int(transition_data[2])
+                transitions.append((start_state, symbol, terminal_state))
+
+            self.alphabet = alphabet_size
+            self.states = states
+            self.transitions = transitions
+
+
+    def __str__(self):
+
+        output_text = ""
+
+        # Collecte de tous les symboles uniques
+        symbols = sorted(set(transition[1] for transition in self.transitions))
+
+        # Création des en-têtes de colonnes
+        headers = ["État"] + symbols
+
+        # Affichage de la première ligne
+        output_text += ("┌─────────" + "┬─────────" * len(symbols) + "┐\n")
+
+        # Affichage des en-têtes
+        output_text += "│"
+        for header in headers:
+                output_text += (header.center(9) + "│")
+        output_text += "\n"
+
+        # Affichage de la ligne de séparation
+        output_text += ("├─────────" + "┼─────────" * len(symbols) + "┤\n")
+
+        # Affichage des lignes de transition
+        for etat in self.states:
+            output_text += "│"
+            output_text += (str(etat).center(9) + "│")
+            for symbol in symbols:
+                transitions = [transition[2] for transition in self.transitions if transition[0] == etat and transition[1] == symbol]
+                if transitions:
+                    output_text += (",".join(map(str, transitions)).center(9) + "│")
+                else:
+                    output_text += ("-".center(9) + "│")
+            output_text += "\n"
+
+        # Affichage de la ligne de fin
+        output_text += ("└─────────" + "┴─────────" * len(symbols) + "┘")
+
+        return output_text
+
+
--- main.py
+++ main.py
@@ -1,67 +1,8 @@
-class Automate:
-    def __init__(self, alphabet, etats, etats_initiaux, etats_terminaux, transitions):
-        self.alphabet = alphabet
-        self.etats = etats
-        self.etats_initiaux = etats_initiaux
-        self.etats_terminaux = etats_terminaux
-        self.transitions = transitions
-
-def lire_automate(filename):
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        alphabet_size = int(lines[0])
-        etats_size = int(lines[1])
-        etats = list(range(etats_size))  # Crée une liste d'états
-        etats_initiaux = list(map(int, lines[2].split()[1:]))
-        etats_terminaux = list(map(int, lines[3].split()[1:]))
-        transitions = []
-        for line in lines[5:]:
-            transition_data = line.strip()
-            etat_depart = int(transition_data[0])
-            symbole = transition_data[1]
-            etat_arrivee = int(transition_data[2])
-            transitions.append((etat_depart, symbole, etat_arrivee))
-        return Automate(alphabet_size, etats, etats_initiaux, etats_terminaux, transitions)
-
-def afficher_automate(automate):
-    # Collecte de tous les symboles uniques
-    symboles = sorted(set(transition[1] for transition in automate.transitions))
-
-    # Création des en-têtes de colonnes
-    headers = ["État"] + symboles
-
-    # Affichage de la première ligne
-    print("┌─────────" + "┬─────────" * len(symboles) + "┐")
-
-    # Affichage des en-têtes
-    print("│", end="")
-    for header in headers:
-        print(header.center(9), end="│")
-    print("")
-
-    # Affichage de la ligne de séparation
-    print("├─────────" + "┼─────────" * len(symboles) + "┤")
-
-    # Affichage des lignes de transition
-    for etat in automate.etats:
-        print("│", end="")
-        print(str(etat).center(9), end="│")
-        for symbole in symboles:
-            transitions = [transition[2] for transition in automate.transitions if transition[0] == etat and transition[1] == symbole]
-            if transitions:
-                print(",".join(map(str, transitions)).center(9), end="│")
-            else:
-                print("-".center(9), end="│")
-        print("")
-
-    # Affichage de la ligne de fin
-    print("└─────────" + "┴─────────" * len(symboles) + "┘")
-
+from automaton import Automaton
 def main():
     filename = input("Entrez le nom du fichier contenant l'automate : ")
-    automate = lire_automate(filename)
-    afficher_automate(automate)
+    automaton1 = Automaton(filename)
+    print(automaton1)
 
 if __name__ == "__main__":
     main()
-
