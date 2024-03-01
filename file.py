96e16094f9eb40c10fa68275c9cfa3918cc3df05
Lakkas <abdel-waheb.sakkal@efrei.net>
2024-03-01 16:03:53 +0100

    adding tkinter for file choice

---
+++ file.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import filedialog
+
+file = "."
+
+def file_choice():
+    global file
+    file = filedialog.askopenfilename()
+    if file:
+        label.configure(text="Fichier sélectionné : " + file)
+        validation_btn.config(state=tk.NORMAL)
+    else:
+        label.configure(text="Aucun fichier sélectionné.")
+        validation_btn.config(state=tk.DISABLED)
+
+def finish():
+    root.destroy()
+
+root = tk.Tk()
+root.title("Sélection du fichier de l'automate")
+
+file_selection_btn = tk.Button(root, text="Sélectionner un fichier", command=file_choice)
+file_selection_btn.pack(pady=20)
+
+label = tk.Label(root, text="")
+label.pack()
+
+# Bouton de validation
+validation_btn = tk.Button(root, text="Valider et Quitter", command=finish, state=tk.DISABLED)
+validation_btn.pack(pady=10)
+
+if __name__ == "__main__":
+    root.mainloop()
--- main.py
+++ main.py
@@ -1,7 +1,55 @@
 from automaton import Automaton
+import file
+import os
+
+
+def file_input():
+
+    is_input_valid = False
+
+    while not is_input_valid:
+        input_text = input("Entrez le nom du fichier contenant l'automate : ")
+        if not input_text.endswith('.txt') and '?' not in input_text:
+            print("La saisie n'est pas valide. Veuillez réessayer.")
+        elif input_text.endswith('.txt') and not os.path.exists(input_text):
+            print("Le fichier spécifié n'existe pas. Veuillez réessayer.")
+        else:
+            is_input_valid = True
+
+    if input_text == "?":
+
+        file.root.mainloop()
+        input_text = file.file
+
+        if not input_text.endswith('.txt'):
+            print("Le fichier spécifié n'est pas valide. Veuillez réessayer.")
+            input_text = file_input()
+
+
+
+    return input_text
+
+def validate_input(input_text):
+
+    is_input_valid = False
+
+    while 1:
+        validation_input = input("Voulez-vous valider l'automate ? (o/n) : ")
+        if validation_input not in ["o", "n"]:
+            print("La saisie n'est pas valide. Veuillez réessayer.")
+        elif validation_input == "o":
+            return True
+        elif validation_input == "n":
+            return False
+
+
 def main():
-    filename = input("Entrez le nom du fichier contenant l'automate : ")
-    automaton1 = Automaton(filename)
+
+    input_text = file_input()
+    while not validate_input(input_text):
+        input_text = file_input()
+
+    automaton1 = Automaton(input_text)
     print(automaton1)
 
 if __name__ == "__main__":
