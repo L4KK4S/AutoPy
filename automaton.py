65da8059ab58c6902a50f1626682a56818c90e39
Anaelle <anaelle.pollart@gmail.com>
2024-03-06 08:52:06 +0100

    fonction is_standard

--- automaton.py
+++ automaton.py
@@ -73,6 +73,7 @@ class Automaton:
     def is_standard(self):
         #Vérification qu'on a un unique état initial
         if len(self.initals_states) != 1: #on regarde si on a un nombre d'états initiaux différent de 1
+            print("L'automate n'est pas standard")
             return False # L'automate est non standard
 
         initial_state = self.initals_states[0] #stock l'unique état initial dans la variable initial_state
@@ -80,7 +81,9 @@ class Automaton:
         # Vérification qu'il n'y a aucune transition menant à l'unique état initial
         for transition in self.transitions: #on parcourt toutes les transitions
             if transition[2] == initial_state: #on regarde si l'état d'arrivée de la transition correspond à l'état initial
+                print("L'automate n'est pas standard")
                 return False
 
         # Si les deux conditions sont remplies, l'automate est standard
+        print("L'automate est standard")
         return True
